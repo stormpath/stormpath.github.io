68984dbd6cd7e27389c6e7ca758e3cce37c2e4c3
Tom Abbott <tom@stormpath.com>
2015-04-27 14:48:04 -0700

    Site updated at 2015-04-27 21:48:04 UTC

--- angularjs/sdk/js/docs-setup.js
+++ angularjs/sdk/js/docs-setup.js
@@ -11,16 +11,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "API Documentation",
       "shortDescription": "stormpath-sdk-angularjs module for AngularJS",
-      "keywords": "$stormpath _blank ability access accesstokenttl account adding allow alternate angular angularjs api application applications article attacks authenticate authentication backed backend becuase best cache class code comprehensive configure conjuction controlled cookie cookies creation current declare default designed dive documentation endpoints environment espeically example expects expiration expire express express-based features field find flag form github guide heard hide href http-only https insecure integration intended issue issued javascript js jti jwt label label-warning langues level login logs maintain mechanism module moments node nonce object options overview parts password place production project protect provider quickstart reading ready repo response revocation revoke rules sdk secure secure-only sense server services session set single specific started stateless statement storage store stored stormpath stormpath-sdk-angularjs support target token tokens truth ttl tutorial ui ui-router ui-state unique urls user username vague valid walkthrough works written xss"
-    },
-    {
-      "section": "api",
-      "id": "stormpath",
-      "shortName": "stormpath",
-      "type": "overview",
-      "moduleName": "stormpath",
-      "shortDescription": "Welcome!  Let&#39;s get started",
-      "keywords": "add angular angularjs api application bower configuration core default defaults depenency directives dist document documentation file files folder forms github include includes inside install installation io issues javascript js json kinds latest libray login main management manual manually min module moments myapp option optional override overrides overview package reading registration remove rest scripts sdk sense simply sipmply sovling src started stormpath stormpath-sdk-angularjs templates tpls user var view"
+      "keywords": "$stormpath _blank ability access accesstokenttl account adding allow alternate angular angularjs api application applications article attacks authenticate authentication backed backend best cache class code comprehensive configure conjunction controlled cookie cookies creation current declare default designed dive documentation endpoints environment example expects expiration expire express express-based features field find flag form github guide heard hide href http-only https insecure integration intended issue issued javascript js jti jwt label label-warning languages level login logs maintain mechanism module moments node nonce object options overview parts password place production project protect provider quickstart reading ready repo response revocation revoke rules sdk secure secure-only sense server services session set single specific started stateless statement storage store stored stormpath stormpath-sdk-angularjs support target token tokens truth ttl tutorial ui ui-router ui-state unique urls user username vague valid walkthrough works written xss"
     },
     {
       "section": "api",
@@ -29,7 +20,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "stormpath",
       "shortDescription": "This service allows you to enable application-wide features of the library.",
-      "keywords": "$auth $authenticated $stormpath access allows alternatively angular api application-wide authservice autoredirect call config configurations control defaultpostloginstate defaults define defined directly enable enabled event events_ examples features function happen integration library ll login logins loginstate main method module myapp object originally redirected requested router send service spstateconfig stormpath successful true ui uirouter user visited"
+      "keywords": "$auth $authenticated $stormpath access allows alternatively angular api application-wide authservice autoredirect call config configurations control defaultpostloginstate defaults define defined directly enable enabled event events_ examples features function happen integration library ll login loginstate logs main method module myapp object originally redirected requested router send service spstateconfig stormpath successful true ui uirouter user visited"
     },
     {
       "section": "api",
@@ -37,7 +28,7 @@ NG_DOCS={
       "shortName": "stormpath.authService",
       "type": "overview",
       "moduleName": "stormpath.authService",
-      "shortDescription": "This module provides the $auth service",
+      "shortDescription": "This module provides the $auth service.",
       "keywords": "$auth api authservice configuration currently methods module overview provider service stormpath"
     },
     {
@@ -47,7 +38,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "stormpath.authService",
       "shortDescription": "The auth service provides methods for authenticating a user, aka",
-      "keywords": "$auth $http $scope $state access aka api authenticate authenticating authntication authservice backend catch console controller cookie credential credentialdata credentials data email error errormessage example expose field form formdata function handler http-only literal log logging login loginctrl message method methods myapp ng-submit null object objects passing password promise provided provider resolved response return sends server service social store stormpath success token user username usernmae verify"
+      "keywords": "$auth $http $scope $state access aka api authenticate authenticating authentication authservice backend catch console controller cookie credential credentialdata credentials data email error errormessage example expose field form formdata function handler http-only literal log logging login loginctrl message method methods myapp ng-submit null object objects passing password promise provided provider resolved response return sends server service social stored stormpath success token user username verify"
     },
     {
       "section": "api",
@@ -55,7 +46,7 @@ NG_DOCS={
       "shortName": "stormpath.authService.$authProvider",
       "type": "object",
       "moduleName": "stormpath.authService",
-      "shortDescription": "Provides the $auth service",
+      "shortDescription": "Provides the $auth service.",
       "keywords": "$auth $authprovider api authservice configuration currently methods object provider service stormpath"
     },
     {
@@ -64,7 +55,7 @@ NG_DOCS={
       "shortName": "ifNotUser",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "Use this directive to conditionally show an element, if the user is NOT logged in.",
+      "shortDescription": "Use this directive to conditionally show an element if the user is NOT logged in.",
       "keywords": "api class conditionally container directive element if-not-user ifnotuser logged login stormpath user"
     },
     {
@@ -73,7 +64,7 @@ NG_DOCS={
       "shortName": "ifUser",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "Use this directive to conditionally show an element, if the user is logged in.",
+      "shortDescription": "Use this directive to conditionally show an element if the user is logged in.",
       "keywords": "api class conditionally container directive element fullname if-user ifuser logged stormpath user"
     },
     {
@@ -109,7 +100,7 @@ NG_DOCS={
       "shortName": "ifUserStateUnknown",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "Use this directive to show an element, while waiting to know if the user",
+      "shortDescription": "Use this directive to show an element while waiting to know if the user",
       "keywords": "api application directive element graphic if-user-state-unknown ifuserstateunknown loading logged stormpath user waiting"
     },
     {
@@ -127,7 +118,7 @@ NG_DOCS={
       "shortName": "spLoginForm",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "This directive will render a pre-built login form, with all",
+      "shortDescription": "This directive will render a pre-built login form with all",
       "keywords": "$auth $authenticated $stormpath alternate api authservice choice class configured container default directive event events_ fields fired form happen html integration login methods_uirouter originally pre-built render requested router sp-login-form sploginform stormpath success template template-url ui url user view"
     },
     {
@@ -136,7 +127,7 @@ NG_DOCS={
       "shortName": "spLogout",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "This directive adds a click handler to the element.  When clicked the user will be logged out.",
+      "shortDescription": "This directive adds a click handler to the element.  When clicked, the user will be logged out.",
       "keywords": "adds api click clicked directive element handler logged main sp-logout splogout stormpath ui-sref user"
     },
     {
@@ -154,8 +145,8 @@ NG_DOCS={
       "shortName": "spPasswordResetRequestForm",
       "type": "directive",
       "moduleName": "stormpath",
-      "shortDescription": "This directive will render a pre-built form which prompts the user for thier",
-      "keywords": "account alternate api class container default directive email form html link password pre-built prompts render reset send sp-password-reset-request-form sppasswordresetrequestform stormpath template template-url thier url user username"
+      "shortDescription": "This directive will render a pre-built form which prompts the user for their",
+      "keywords": "account alternate api class container default directive email form html link password pre-built prompts render reset send sp-password-reset-request-form sppasswordresetrequestform stormpath template template-url url user username"
     },
     {
       "section": "api",
@@ -164,7 +155,7 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "stormpath",
       "shortDescription": "This directive will render a pre-built user registration form with the following",
-      "keywords": "$auth $authenticated $user _blank account add alternate api application authenticate authservice autologin automatically call check class container copy create created creation custom customdata customize customizing data default directive directory email enabled event events_ false field fields file fit form formmodel href html https inputs interaction login main making message method methods_authenticate methods_create middlename modify ng-model option optionally options password point post post-login-state postloginstate pre-built ready redirect redirected register registration render server set sp-registration-form spregistrationform stormpath target telling template template-url tpl trigger ui url user userservice verification view workflow"
+      "keywords": "$auth $authenticated $user _blank account add alternate api application authenticate authservice autologin automatically call check class container copy create created creation custom customdata customize customizing data default directive directory email enabled event events_ false field fields file fit form formmodel href html https inputs interaction login main making message method methods_authenticate methods_create middlename modify ng-model option optionally options password point post post-login-state postloginstate pre-built ready redirect redirected registered registration render server set sp-registration-form spregistrationform stormpath target telling template template-url tpl trigger ui url user userservice verification view workflow"
     },
     {
       "section": "api",
@@ -173,7 +164,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "stormpath",
       "shortDescription": "The Stormpath State Config is an object that you can define on a",
-      "keywords": "$stateprovider $stormpath access adminctrl admins angular api authenticate authenticated authentication authorize calling check class config control controller currently define defines delay depending enable example group group-based homepage integration logged login main methods_uirouter module myapp object order originally redicted redircted requested require router rules secrets secretsctrl showing situations sp spstateconfig stormpath stormpath-spstateconfig-page stormpath-spstateconfig-spstateconfig-page supports transition true ui uirouter url user view wait waitforuser"
+      "keywords": "$stateprovider $stormpath access adminctrl admins angular api authenticate authenticated authentication authorize calling check class config control controller currently define defines delay depending enable example group group-based homepage integration logged login main methods_uirouter module myapp object order originally redirected requested require router rules secrets secretsctrl showing situations sp spstateconfig stormpath stormpath-spstateconfig-page stormpath-spstateconfig-spstateconfig-page supports transition true ui uirouter url user view wait waitforuser"
     },
     {
       "section": "api",
@@ -181,7 +172,7 @@ NG_DOCS={
       "shortName": "stormpath.userService",
       "type": "overview",
       "moduleName": "stormpath.userService",
-      "shortDescription": "This module provides the $user service",
+      "shortDescription": "This module provides the $user service.",
       "keywords": "$user api module overview service stormpath userservice"
     },
     {
@@ -191,7 +182,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "stormpath.userService",
       "shortDescription": "Use this service to get the current user and do access control checks",
-      "keywords": "$currentuser $http $notloggedin $scope $user _blank access account accountdata address angular api application attach attemps attempt authenticated authenticating automatically boolean case catch checks config console control controller create created current data directory eh email emitted enabled endpoint error errormessage events expects express failure false fields function givenname handled href http https js json literal log meet message method middleware modified module myapp myappctrl object obseve occurs operation option passed passing password post proceed promise ready register rejected representing require required requirements requires resolved response responses returns sdk send service simply status stormmpath stormpath submitting success successful surname target true type unverified user user_collection_uri userservice var verification weak wishes workflow wrapped"
+      "keywords": "$currentuser $http $notloggedin $scope $user _blank access account accountdata address angular api application attach attempt attempts authenticated authenticating automatically boolean case catch checks config console control controller create created current data directory eh email emitted enabled endpoint error errormessage events expects express failure false fields function givenname handled href http https js json literal log meet message method middleware modified module myapp myappctrl object observe occurs operation option passed passing password post proceed promise ready register rejected representing require required requirements requires resolved response responses returns sdk send service simply status stormpath submitting success successful surname target true type unverified user user_collection_uri userservice var verification weak wishes workflow wrapped"
     },
     {
       "section": "api",
@@ -199,7 +190,7 @@ NG_DOCS={
       "shortName": "stormpath.userService.$userProvider",
       "type": "object",
       "moduleName": "stormpath.userService",
-      "shortDescription": "Provides the $user service",
+      "shortDescription": "Provides the $user service.",
       "keywords": "$user $userprovider api configuration currently methods object provider service stormpath userservice"
     },
     {
@@ -218,7 +209,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Get Started",
       "shortDescription": "Installation - Bower",
-      "keywords": "add angular application bower configuration core default defaults depenency directives dist document file files folder forms github include includes inside install installation io javascript js json latest libray login main manual manually min module myapp option optional override overrides overview package quickstart registration remove rest scripts sense simply sipmply src started stormpath stormpath-sdk-angularjs templates tpls var view"
+      "keywords": "add angular application bower configuration core default defaults dependency directives dist document file files folder forms github include includes inside install installation io javascript js json latest library login main manual manually min module myapp option optional override overrides overview package quickstart registration remove rest scripts sense simply src started stormpath stormpath-sdk-angularjs templates tpls var view"
     }
   ],
   "apis": {
