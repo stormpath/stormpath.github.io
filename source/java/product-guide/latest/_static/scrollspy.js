fbc0a0b0b0da6c53f2a4a465d6e5188d640f3f07
stormpath-documentation Auto Doc Build <evangelists@stormpath.com>
2016-06-17 19:13:54 +0000

    stormpath-documentation release 1.1.2

--- source/java/product-guide/latest/_static/scrollspy.js
+++
@@ -1,8 +0,0 @@
- $(document).ready(function() {
-   $('div.wy-menu ul li ul').attr('data-gumshoe', '');
-
-   gumshoe.init({
-     selector: "[data-gumshoe] li.toctree-l2 > a",
-     activeClass: 'current'
-   });
- });
